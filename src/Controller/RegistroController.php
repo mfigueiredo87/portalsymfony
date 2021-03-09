91e93fa53c5d6220bc35a0b54182587c9eec6f0f
mfigueiredo87 <figtch@gmail.com>
2021-03-09 15:47:55 +0000

    Encriptando senha

--- src/Controller/RegistroController.php
+++ src/Controller/RegistroController.php
@@ -8,11 +8,13 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+//para encriptar a senha
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class RegistroController extends AbstractController
 {
     #[Route('/registro', name: 'registro')]
-    public function index(Request $request): Response
+    public function index(Request $request, UserPasswordEncoderInterface $passwordEncoder): Response
     {
         $user = new User();
         $form = $this->createForm(type: UserType::class, data: $user);
@@ -21,6 +23,8 @@ class RegistroController extends AbstractController
             $em = $this->getDoctrine()->getManager();
             $user->setBanido(banido: false);
             $user->setRoles(['ROLE_USER']);
+//          <!--Encriptando a senha-->
+            $user->setPassword($passwordEncoder->encodePassword($user,$form['password']->getData()));
             $em ->persist($user);
             $em ->flush();
             $this->addFlash('exito',message:'Operação realizada com sucesso!');
